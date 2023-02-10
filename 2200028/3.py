e18b6fb5b36a6d0485b448ccffd20f02e8885dc2
syyoung <yuyoung.dev@gmail.com>
2023-02-10 17:01:17 +0900

    write

---
+++ 2200028/3.py
@@ -0,0 +1,13 @@
+import sys
+sys.stdin = open("input3.txt", "r")
+
+vowel = ['a', 'e','i','o','u']\
+
+T = int(input())
+for t in range(1, T+1):
+    result = ''
+    word = input()
+    for i in word:
+        if i not in vowel:
+            result += i
+    print(f'#{t} {result}')
---
+++ 2200028/5.py
@@ -0,0 +1,5 @@
+import sys
+sys.stdin = open("input5.txt", "r")
+
+T = int(input())
+for t in range(1, T+1):
