893958e71fc47a21588c8bf064e0d99a429b91cb
thebjko <thebjko@gmail.com>
2023-02-12 18:38:43 +0900

    minor refactor 1949. 등산로 조성

--- 2200018/1949.py
+++ 2200018/1949.py
@@ -3,49 +3,43 @@ stdin = open("input_1949.txt")
 # stdin = open("sample_input.txt")
 input = stdin.readline
 
-def dfs(start, matrix, depth, construction):
+def dfs(start, matrix, depth, K):
     global result
-    c = construction
-    d = depth
     (x, y) = start
     height = matrix[y][x]
-
-    if height == "X":
-        return d
 
     for i in r(4):
         a = x + dx[i]
         b = y + dy[i]
         if a < 0 or b < 0 or a >= N or b >= N:
             continue
-        else:
-            if matrix[b][a] == "X":
-                continue
-            if matrix[b][a] < height:
-                """
-                공사를 하지 않는 경우
-                """
-                # 서로 영향을 받지 않는 지도 케이스 생성(shallow copy 활용)
-                m = [row.copy() for row in matrix]
-                m[y][x] = "X"
-                route = dfs((a, b), m, d + 1, c)
-                if route > result:
-                    result = route
-            elif matrix[b][a] - height < c:
-                """
-                다음 구간이 현재 구간보다 크거나 같지만 다음 구간을 1~c 만큼 줄이면 지나갈 수 있는 경우
-                """
-                m = [row.copy() for row in matrix]
-                m[b][a] = m[y][x] - 1
-                m[y][x] = "X"
-                route = dfs((a, b), m, d + 1, 0)
-                if route > result:
-                    result = route
+        if matrix[b][a] == "X":
+            continue
+        if matrix[b][a] < height:
+            """
+            공사를 하지 않는 경우
+            """
+            # 서로 영향을 받지 않는 지도 케이스 생성(shallow copy 활용)
+            m = [row.copy() for row in matrix]
+            m[y][x] = "X"
+            route = dfs((a, b), m, depth+1, K=K)
+            if route > result:
+                result = route
+        elif matrix[b][a] - height < K:
+            """
+            다음 구간이 현재 구간보다 크거나 같지만 다음 구간을 1~c 만큼 줄이면 지나갈 수 있는 경우
+            """
+            m = [row.copy() for row in matrix]
+            m[b][a] = m[y][x] - 1   # 최소 필요 공사 크기
+            m[y][x] = "X"   # 공사 후 방문표시
+            route = dfs((a, b), m, depth+1, K=0)
+            if route > result:
+                result = route
     else:
         """
         진행할 수 없는 경우
         """
-        return d
+        return depth
 
 r = range
 for i in r(int(input())):
@@ -63,6 +57,6 @@ for i in r(int(input())):
 
     # 각 꼭대기에서 dfs 실행
     for apex in apexes:
-        dfs(apex, matrix, 1, K)
+        dfs(apex, matrix, depth=1, K=K)
 
     print(f"#{i + 1}", result)
